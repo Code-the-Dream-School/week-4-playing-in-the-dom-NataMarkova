4cce533b706a1ca6775afd12499d085b01a6d788
Natalia Markova <nataliamarkova@Admins-MacBook-Air.local>
2020-07-14 19:13:02 -0700

    Completed assignment / week 4

--- index.js
+++ index.js
@@ -7,7 +7,20 @@
 
 //resolve // QUESTION 1 here
 
+var newDiv = document.createElement('div');
+var a1Div = document.getElementById('a-1');
+a1Div.append(newDiv);
 
+newDiv.className = 'square';
+newDiv.innerText = 'x';
+
+newDiv.addEventListener('click', () =>  {
+    if (newDiv.innerText === 'x') {
+        newDiv.innerText = 'o';
+    } else {
+        newDiv.innerText = 'x';
+    }
+});
 
 
 //------------------------Question 2 ---------------------------
@@ -16,16 +29,34 @@
 //you also need to create a button that makes the remove action of the element selected
 // append the new list as a child of the element with the 'id = a-2'
 // append the button as a child of the element with the 'id = a-2'
-const colors = [ 'red' , 'white', 'black' , 'green' , 'orange'];
 
+// declare the variables
+const colors = ['red' , 'white', 'black' , 'green' , 'orange'];
+const select = document.createElement('select');
+const a2Div = document.getElementById('a-2');
 
+// using for loop create dropdown list
+for (i = 0; i < colors.length; i++) {
+    let optionValue = document.createElement('option');
+    optionValue.value = colors[i];
+    optionValue.innerText = colors[i];
+    select.appendChild(optionValue);
+}
 
+// append the new list as a child of the element with the 'id = a-2'
+a2Div.appendChild(select);
 
-//resolve // QUESTION 2 here
+// create Remove button
+const removeButton = document.createElement('button');
+removeButton.innerText = 'Remove';
 
+// removing elements bi the index in the colors list
+removeButton.addEventListener('click', () => {
+    select.options[select.selectedIndex].remove();
+})
 
-
-
+// append the button as a child of the element with the 'id = a-2'
+a2Div.appendChild(removeButton);
 
 
 //------------------------Question 3 ---------------------------
@@ -34,13 +65,35 @@ const colors = [ 'red' , 'white', 'black' , 'green' , 'orange'];
 //And display the results in the elements with the 'id = volume' and 'id = area' respectively
 //hint: the volumen of a sphere is ((4/3) × π × r^3) and the surface area is (4 × π × r^2)
 
+// declare the variables that is going to use in function
+const radiusField = document.getElementById('radius');
+const volumeField = document.getElementById('volume');
+const areaField = document.getElementById('area');
 
-const calculate_sphere = () =>{
+const calculate_sphere = () => {
 
+    // input the radius value 
+    let radius = radiusField.value;
+
+    // calculate the volume and area value
+    let volume = ((4/3) * Math.PI * (radius^3));
+    let area = (4 * Math.PI * (radius^2));
+    
+    // providing the results of calculation 
+    volumeField.value = volume;
+    areaField.value = area;
+
+    // debugging purpose
+    console.log(volume);
+    console.log(area);
+
+    // prevent from default browser settings
+    return false
  }
 
-window.onload = document.getElementById('MyForm').onsubmit = calculate_sphere; // this execute the volume_sphere function each time the calculate (submit) button is clicked
+document.getElementById('MyForm').onsubmit = calculate_sphere;
 
+// this execute the volume_sphere function each time the calculate (submit) button is clicked
 
 
 
@@ -48,5 +101,31 @@ window.onload = document.getElementById('MyForm').onsubmit = calculate_sphere; /
 //Now in this Exercise we want you to create 3 buttons wich clicks actions are going to hide the respective question above with their aswer and if they are hidden then the click option should be display the question with their respective aswer
 // append the 3 buttons to the element with the 'id = a-4' as childrens
 
+// create variables
+const quest1 = document.getElementById('a-1');
+const quest2 = document.getElementById('a-2');
+const quest3 = document.getElementById('a-3');
+const a4Div = document.getElementById('a-4');
+let answers = [quest1, quest2, quest3];
 
-//resolve // QUESTION 4 here
+// create buttons that are refer to the array answers
+for (i = 0; i < answers.length; i++) {
+    let button1 = document.createElement('button');
+    let textInside = document.createTextNode(`Hide Question ${i + 1}`);
+    button1.value = i;
+    button1.appendChild(textInside);
+    a4Div.appendChild(button1);
+}
+
+// hide the answers when user clicks
+a4Div.addEventListener('click', (event) => {
+    const question1 = answers[event.target.value];
+
+// use conditional statement for showing or hiding the questions
+    if (question1.style.display === 'none') {
+        // set the display property to value 'block'
+            question1.style.display = 'block';
+        } else {
+            question1.style.display = 'none';
+        }
+});
